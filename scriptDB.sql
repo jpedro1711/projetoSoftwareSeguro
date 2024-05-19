d460701ab2e37833613bce16eeda86c751143072
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2024-05-19 11:09:28 -0300

    update script: inserts and queries

--- scriptDB.sql
+++ scriptDB.sql
@@ -11,17 +11,12 @@ create table produto (
     quantidadeMinimaEstoque int
 );
 
-INSERT INTO produto (nomeProduto, valorVenda, custoUnitario, quantidadeEstoque, quantidadeMinimaEstoque)
-VALUES ('Produto Exemplo', 100.00, 50.00, 200, 50);
-
-select * from produto;
-# exemplo de date: '2024-05-07'
 create table venda (
 	id int primary key auto_increment,
     dataVenda date ,
     valorTotal decimal
 );
-select * from venda;
+
 create table items_venda (
     id int primary key auto_increment,
     venda_id int,
@@ -30,4 +25,47 @@ create table items_venda (
     foreign key (venda_id) references venda(id),
     foreign key (produto_id) references produto(id)
 );
+
+INSERT INTO produto (nomeProduto, valorVenda, custoUnitario, quantidadeEstoque, quantidadeMinimaEstoque)
+VALUES 
+('Produto Exemplo', 100.00, 50.00, 200, 50),
+('Produto Exemplo2', 100.00, 50.00, 40, 50),
+('Produto Exemplo3', 150.00, 70.00, 100, 30),
+('Produto Exemplo4', 200.00, 100.00, 300, 80),
+('Produto Exemplo5', 250.00, 120.00, 50, 20);
+
+INSERT INTO venda (dataVenda, valorTotal)
+VALUES 
+('2024-05-07', 500.00),
+('2024-05-08', 700.00),
+('2024-05-09', 200.00),
+('2024-05-10', 300.00);
+
+
+INSERT INTO items_venda (venda_id, produto_id, quantidade)
+VALUES 
+(1, 1, 5),
+(1, 2, 2),
+(2, 1, 3),
+(2, 3, 7),
+(3, 4, 1),
+(3, 5, 4),
+(4, 1, 6),
+(4, 2, 5),
+(4, 3, 2);
+
 select * from items_venda;
+
+select * from produto
+where quantidadeEstoque < quantidadeMinimaEstoque;
+
+select sum(valorTotal) from venda;
+
+select produto.NomeProduto, count(*) as contagem from produto
+inner join items_venda as a
+on a.produto_id = produto.id
+inner join venda as b
+on b.id = a.venda_id
+group by produto_id
+order by contagem DESC
+limit 3;
